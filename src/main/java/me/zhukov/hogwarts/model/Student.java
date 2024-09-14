2763a2fd037f26aeb2db22c6d89f18cf627676a3
Александр Жуков <a.zhukov@uzum.com>
2024-09-14 09:31:05 +0200

    bd in progress

--- src/main/java/me/zhukov/hogwarts/model/Student.java
+++ src/main/java/me/zhukov/hogwarts/model/Student.java
@@ -1,12 +1,17 @@
 package me.zhukov.hogwarts.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.util.Objects;
 
+@Entity
 public class Student {
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int age;
